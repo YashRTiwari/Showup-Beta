4fe36877b304a830a95c6bfed22ebf4f29cebad2
YashRTiwari <yrtiean@gmail.com>
2021-02-09 16:58:25 -0600

    something

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ import { createFirestoreInstance } from "redux-firestore";
 import thunk from "redux-thunk";
 
 function App() {
-	const store = createStore(
-		rootReducer,
-		compose(
-			applyMiddleware(thunk),
-			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-		)
-	);
+	const store = createStore(rootReducer, applyMiddleware(thunk));
 
 	// ,
 
